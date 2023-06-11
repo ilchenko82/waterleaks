ab4fb1d1136cc8b95b24696065b2a8da564c8a59
ilchenko82 <s.ilchenko.mb@gmail.com>
2023-06-12 01:11:06 +0300

    11

---
+++ js/swiper-init.js
@@ -0,0 +1,31 @@
+
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+		slidesPerView: 1,
+		breakpoints: {
+			320: {
+			  slidesPerView: 1,
+			  spaceBetween: 20,
+			},
+			768: {
+			  slidesPerView: 2,
+			  spaceBetween: 30,
+			},
+			1100: {
+			  slidesPerView: 3,
+			  spaceBetween: 30,
+			},
+		 }
+
+
+
+    });
