b63bed9e10007875b076b01f37ca780e06d5e1d0
Andrew Sergeyev <andy@isd-group.com>
2023-06-21 01:43:55 +0300

    bugfixxxing

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -12,7 +12,7 @@
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -23,6 +23,12 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    // bodyScrollLock.enableBodyScroll(document.body);
   });
+
+  const mobileMenuItems = document.querySelectorAll('.mobile-menu .mobile-menu__link');
+  mobileMenuItems.forEach(function(el, i, parent) {
+    el.addEventListener('click', toggleMenu);
+  });
+
 })();
