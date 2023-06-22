9458c48c9d8700efe081ab833c8d7139ec31a094
Andrew Sergeyev <andy@isd-group.com>
2023-06-22 23:59:28 +0300

    form sending

--- send.php
+++ send.php
@@ -1,15 +1,42 @@
 <?php
 
     if(!empty($_POST['tel'])) {
-        mail("smnglf@gmail.com", "Waterleaks: new request",
+        $r = mail("s.ilchenko.mb@gmail.com", "Waterleaks: new request",
             "Date: " . date("Y-m-d H:i:s") . "\n" .
             "Phone: " . $_POST['tel'] . "\n" .
-            !empty($_POST['name'] ? ("Name: " . $_POST['name'] . "\n") : "") .
-            !empty($_POST['name'] ? ("Comment: " . $_POST['comment'] . "\n") : "") .
-            !empty($_POST['telegram'] ? ("Telegram: +" . "\n") : "") .
-            !empty($_POST['viber'] ? ("Viber: +" . "\n") : "") .
-            !empty($_POST['phone'] ? ("Phone: +" . "\n") : "") .
-            "",
-            "From: mail@waterleaks.com.ua"
+            (!empty($_POST['name']) ? ("Name: " . $_POST['name'] . "\n") : "") .
+            (!empty($_POST['telegram']) ? ("Telegram: +" . "\n") : "") .
+            (!empty($_POST['viber']) ? ("Viber: +" . "\n") : "") .
+            (!empty($_POST['phone']) ? ("Phone: +" . "\n") : "") .
+            (!empty($_POST['comment']) ? ("Comment: " . $_POST['comment'] . "\n") : "") .
+            ""
         );
+?>
+<html>
+<body>
+<style type="text/css">
+    * { font-family: Roboto,sans-serif; }
+</style>
+<?php
+        if($r) {
+?>
+</body>
+<h1>Запит відправлено! Зв'яжемось з вами найближчим часом.</h1>
+
+<?php
+        }
+        else {
+?>
+
+<h1>Вибачте, сталась помилка. Передзвоніть будь-ласка нам за телефоном <a href="tel:+38 098 151 58 02">+38 098 151 58 02</a>.</h1>
+
+<?php
+        }
     }
+?>
+
+<a href="/">Поівернутись на головну</a>
+
+
+</html>
+</html>
